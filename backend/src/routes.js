d19c049d141e3816f013c8ed194404b69ca0e36e
FernPascoal <fernando.pascoal42@gmail.com>
2019-10-06 18:50:31 -0300

    Add models & controllers, new routes & file upload

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+
+const SessionController = require('./controller/SessionController');
+const SpotController = require('./controller/SpotController');
+const DashboardController = require('./controller/DashboardController');
+const BookingController = require('./controller/BookingController');
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
-routes.post('/users', (req, res) => {
-    return res.json(req.body);
-});
+routes.post('/sessions', SessionController.store);
+
+routes.get('/spots', SpotController.index);
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+
+routes.get('/dashboard', DashboardController.show);
+
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb://node_user:BFwcJ1zHZjza9z4Y@omnistack-shard-00-00-ij1qo.mongodb.net:27017,omnistack-shard-00-01-ij1qo.mongodb.net:27017,omnistack-shard-00-02-ij1qo.mongodb.net:27017/omnistack?ssl=true&replicaSet=OmniStack-shard-0&authSource=admin&retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.use(express.json());
 app.use(routes);
